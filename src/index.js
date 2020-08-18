c9a532d2ac1335056cb7412d8b2347dab1b8fd40
Dinh Tran <tranquocdinhcse98@outlook.com>
2020-08-18 12:53:04 +0700

    Init commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '@babel/polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './modules/App';
+import './assets/styles/main.scss';
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssPresetEnv = require('postcss-preset-env');
+
+const devMode = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {mininize: true},
+                }
+            },
+            {
+               test: /\.(png|svg|jpg|gif)$/,
+               use: [
+                   'file-loader'
+               ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            favicon: './src/assets/images/favicon.ico',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+    }
+}
